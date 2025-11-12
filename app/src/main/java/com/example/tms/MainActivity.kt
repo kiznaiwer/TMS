2a348dfb44500f642dd49cbaf80cba5c01862a13
Алексей <1999ctalih@gmail.com>
2025-11-12 10:50:29 +0300

    task 15.3.1

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.tms
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tms.tasks.Activity15
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.buttonTask)
+        button.setOnClickListener {
+            val intent = Intent(this, Activity15::class.java)
+            intent.putExtra("key1", "Hello, Second Activity")
+            startActivity(intent)
         }
     }
 }
