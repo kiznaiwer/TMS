d23304c5e050d3e46d2cb10e27ada6e7c3e5e7f0
Alex Krivyonok <1999ctalih@gmail.com>
2025-12-03 19:54:43 +0300

    26.1

--- app/src/main/java/com/example/tms/MainActivity.kt
+++ app/src/main/java/com/example/tms/MainActivity.kt
@@ -1,45 +1,56 @@
 package com.example.tms
 
-import Adapter
 import android.content.Context
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.tms.Item
 import com.example.tms.R
+import com.example.tms.databinding.ActivityMainBinding
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.jvm.Throws
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: Adapter
-    private lateinit var addButton: Button
-
-    private val items = mutableListOf<Item>()
-
+class MainActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        updateTextViewAuto()
 
-        recyclerView = findViewById(R.id.recycler_view)
-        addButton = findViewById(R.id.add_button)
-
-        adapter = Adapter(items)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        addButton.setOnClickListener {
-            val newItem = Item(
-                title = "Заголовок ${items.size + 1}",
-                description = "Описание элемента ${items.size + 1}"
-            )
-            adapter.addItem(newItem)
-        }
-
-        adapter.onActionClick = { item ->
-            Toast.makeText(this, "Действие для: ${item.title}", Toast.LENGTH_SHORT).show()
+        binding.btnClck.setOnClickListener {
+            updateTextViewButton()
         }
     }
+
+
+    private fun updateTextViewAuto(){
+        lifecycleScope.launch {
+            for (i in 1..10){
+                delay(1000)
+                binding.text.text = "$i"
+            }
+        }
+    }
+
+    private fun updateTextViewButton(){
+        lifecycleScope.launch {
+            for (i in 10 downTo 1){
+                delay(1000)
+                binding.textView.text = "$i"
+            }
+        }
+    }
+
 }
